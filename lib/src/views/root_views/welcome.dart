846325fab324429363438223c40be2a4e50ab85c
Chandu J S <dev@dotsadvertising.net>
2019-11-06 20:08:38 +0400

    ui updates

--- lib/src/views/root_views/welcome.dart
+++ lib/src/views/root_views/welcome.dart
@@ -13,11 +13,16 @@ class WelcomePage extends StatelessWidget {
       children: <Widget>[
         Text(
           'Hi!',
+          style: Theme.of(context).textTheme.display2,
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Welcome',
           style: Theme.of(context).textTheme.display1,
         ),
         SizedBox(height: 50),
         MaterialButton(
-          color: Colors.red,
+          color: Colors.blueAccent,
           textColor: Colors.white,
           onPressed: this.onClick,
           child: Text('START!'),
